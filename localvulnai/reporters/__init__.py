e3a86c899eaa1bd84bbba2899ed55902964bda99
BluHExH <188824290+BluHExH@users.noreply.github.com>
2026-09-06 00:20:30 +0600

    Wire HTML reporter into package exports

--- localvulnai/reporters/__init__.py
+++ localvulnai/reporters/__init__.py
@@ -2,10 +2,12 @@ from .markdown import generate_markdown_report
 from .json_reporter import generate_json_report
 from .sarif import generate_sarif_report
 from .burp_export import generate_burp_friendly
+from .html_report import generate_html_report
 
 __all__ = [
     "generate_markdown_report",
     "generate_json_report",
     "generate_sarif_report",
     "generate_burp_friendly",
+    "generate_html_report",
 ]
